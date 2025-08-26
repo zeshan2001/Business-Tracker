9a52df5536028ed6e51e4bfea5ce5e158bc68ef7
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-26 10:27:20 +0300

    create Request models

--- Business_owner/models.py
+++ Business_owner/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ..Bank_Panel.models import Bank
 
 # Create your models for Business here.
 class Business(models.Model):
@@ -18,9 +19,16 @@ class Balance_sheet(models.Model):
     non_current_liabilities=models.DecimalField(max_digits=12,decimal_places=2)
     shareholders_equity=models.DecimalField(max_digits=12,decimal_places=2)
 
-class income_statement(models.Model):
+class Income_statement(models.Model):
     business_id=models.ForeignKey(Business.business_id,on_delete=models.CASCADE)
     revenue=models.DecimalField(max_digits=12,decimal_places=2)
     cogs=models.DecimalField(max_digits=12,decimal_places=2)
     operating_expenses=models.DecimalField(max_digits=12,decimal_places=2)
     net_income=models.DecimalField(max_digits=12,decimal_places=2)
+
+class Request(models.Model):
+    business_id=models.ForeignKey(Business.business_id,on_delete=models.CASCADE)
+    bank_id=models.ForeignKey(Bank,on_delete=models.CASCADE)
+    borrow_amount=models.DecimalField(max_digits=12,decimal_places=2)
+    description=models.CharField(max_length=255)
+    status=models.CharField(max_length=50)
