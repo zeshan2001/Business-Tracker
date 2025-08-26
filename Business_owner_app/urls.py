0b15c90d742d00c90ba61fa1b6b07eb378c744bd
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-26 22:01:38 +0300

    edit form path

--- Business_owner_app/urls.py
+++ Business_owner_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.business, name="Business"),
     path('<int:business_id>/',views.business_detail, name='business_detail'),
-    path('business/create/',views.business_Create.as_view(),name='business_Create'),
+    path('create/',views.business_Create.as_view(),name='business_Create'),
     path("profile", views.profile, name="Profile")
 ]
