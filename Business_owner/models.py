15cfb9791840d5273ccc91e8f88c83998adc4147
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-26 10:19:32 +0300

    create income_statement models

--- Business_owner/models.py
+++ Business_owner/models.py
@@ -17,4 +17,10 @@ class Balance_sheet(models.Model):
     current_liabilities=models.DecimalField(max_digits=12,decimal_places=2)
     non_current_liabilities=models.DecimalField(max_digits=12,decimal_places=2)
     shareholders_equity=models.DecimalField(max_digits=12,decimal_places=2)
-    
+
+class income_statement(models.Model):
+    business_id=models.ForeignKey(Business.business_id,on_delete=models.CASCADE)
+    revenue=models.DecimalField(max_digits=12,decimal_places=2)
+    cogs=models.DecimalField(max_digits=12,decimal_places=2)
+    operating_expenses=models.DecimalField(max_digits=12,decimal_places=2)
+    net_income=models.DecimalField(max_digits=12,decimal_places=2)
