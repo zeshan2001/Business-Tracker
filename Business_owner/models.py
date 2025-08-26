28df71d790d4b3a56e026c9a270ae1dc2abe2c13
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-26 09:57:09 +0300

    create business models

--- Business_owner/models.py
+++ Business_owner/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models for Business here.
+class Business(models.Model):
+    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    brand=models.CharField(max_length=255)
+
+
+
+
+class Balance_sheet(models.Model):
+    business_id=models.ForeignKey(Business.business_id,on_delete=models.CASCADE)
+    current_assets=models.FloatField()
