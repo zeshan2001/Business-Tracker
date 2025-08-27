b722931d15c9a982016048c9aae90934ec1dc937
Ham33d-754 <mahmoodish873@gmail.com>
2025-08-27 09:34:37 +0300

    test

--- investor_app/urls.py
+++ investor_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("investor/", views.investor, name="investor")
+    path("investor/", views.investor, name="investor"),
+    path("investor/profie", views.investor, name="investor"),
 ]
