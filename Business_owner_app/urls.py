dccb6664c9eded8404b70997a6d63a6b39655fc5
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-27 10:19:46 +0300

    add edit for business

--- Business_owner_app/urls.py
+++ Business_owner_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.business, name="business"),
     path('<int:business_id>/',views.business_detail, name='business_detail'),
     path('create/',views.business_Create.as_view(),name='business_Create'),
+    path('create/<int:pk>/updata/',views.business_Updata.as_view(),name='business_updata'),
     path("profile", views.profile, name="Profile")
 ]
