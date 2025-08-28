de6b62e2f7b9c58a328fbb8be3454e6d0ca2d46c
oklyz <alimodffs2@gmail.com>
2025-08-28 07:54:16 +0300

    save

--- Bank_app/views.py
+++ Bank_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from main_app.models import Bank
+from main_app.decorators import role_required
+from main_app.mixins import RoleRequiredMixin
 # Create your views here.
-
+@role_required(allowed_roles=["B"])
 def bank(request):
+    profile = getattr(request.user, "profile", None)
+    bank = Bank.objects.get(id= profile.bank_id)
     
-    return render(request, "bank.html")
+    return render(request, "bank.html", {"bank": bank})
