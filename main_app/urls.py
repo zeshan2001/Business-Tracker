e590b41eeb041f90b3dd91e2553ff8d67fab1fea
oklyz <alimodffs2@gmail.com>
2025-08-28 19:36:51 +0300

    def payback and def npv and def irr completed

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("contact/", views.contact, name="contact"),
     path("accounts/signup", views.signup, name="signup"),
+    path("test/", views.test, name="test")
 ]
