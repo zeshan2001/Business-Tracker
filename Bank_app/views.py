8706577a5f774dfedec7503c7382738c67f72ca0
oklyz <alimodffs2@gmail.com>
2025-08-28 10:01:05 +0300

    save

--- Bank_app/views.py
+++ Bank_app/views.py
@@ -3,7 +3,7 @@ from main_app.models import Bank
 from main_app.decorators import role_required
 from main_app.mixins import RoleRequiredMixin
 # Create your views here.
-@role_required(allowed_roles=["B"])
+@role_required(allowed_roles=["L"])
 def bank(request):
     profile = getattr(request.user, "profile", None)
     bank = Bank.objects.get(id= profile.bank_id)
