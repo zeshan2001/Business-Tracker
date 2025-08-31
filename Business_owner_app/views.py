f06cd42f56fc198190dc8550b0462fc3c0c4702c
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-31 10:41:27 +0300

    display list of banks

--- Business_owner_app/views.py
+++ Business_owner_app/views.py
@@ -100,18 +100,6 @@ def business(request):
     businesses=Business.objects.filter(user = request.user)
     return render(request, 'business.html',{'businesses':businesses})
 
-# def business_detail(request, business_id):
-#     business = get_object_or_404(Business, id=business_id)
-#     balance_sheets = business.balance_sheets.all()  
-#     income_statements = business.income_statements.all()
-#     context = {
-#         'business': business,
-#         'balance_sheets': balance_sheets,
-#         'income_statements': income_statements,
-#     }
-#     return render(request, 'business_detail.html', context)
-
-
 
 
 
