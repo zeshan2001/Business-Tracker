8d3b08e5968c25abf5c8da75d1b810bc7d024ea3
oklyz <alimodffs2@gmail.com>
2025-08-31 16:08:04 +0300

    when select business id it shows in loan

--- Bank_app/urls.py
+++ Bank_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("request/", views.request_view, name="request"),
     path("request/<int:request_id>", views.request_detail, name="request_detail"),
     path("create_loan/", views.LoanCreate.as_view(), name="create_loan"),
-    path("request/<int:pk>/update", views.RequestUpdate.as_view(), name="request_update")
+    path("request/<int:pk>/update", views.RequestUpdate.as_view(), name="request_update"),
+    path("request-detail/<int:business_id>/", views.RequstDetail.as_view(), name="request_detail_loan")
 ]
